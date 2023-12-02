a0c3575c24cf7b7fbfb6c7b0fe3aa0a6812e60a2
tsultan16 <tanzids@student.unimelb.edu.au>
2023-12-02 13:58:11 +1100

    implemented token and position embedding

--- bert.py
+++ bert.py
@@ -180,15 +180,16 @@ class BertModel(BertPreTrainedModel):
     # Get word embedding from self.word_embedding into input_embeds.
     inputs_embeds = None
     ### TODO
-    raise NotImplementedError
 
+    input_embeds = self.word_embedding(input_ids)
 
     # Get position index and position embedding from self.pos_embedding into pos_embeds.
     pos_ids = self.position_ids[:, :seq_length]
 
     pos_embeds = None
     ### TODO
-    raise NotImplementedError
+
+    pos_embeds = self.pos_embedding(pos_ids)
 
 
     # Get token type ids, since we are not consider token type, just a placeholder.
@@ -197,7 +198,12 @@ class BertModel(BertPreTrainedModel):
 
     # Add three embeddings together; then apply embed_layer_norm and dropout and return.
     ### TODO
-    raise NotImplementedError
+    
+    # combined embedding
+    embeds = input_embeds + pos_embeds + tk_type_embeds
+    # apply layer norm and dropout
+    embeds = self.embed_dropout(self.embed_layer_norm(embeds))
+    return embeds
 
 
   def encode(self, hidden_states, attention_mask):
